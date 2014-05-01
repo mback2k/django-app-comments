aa5e2593656514387585401f6316bd1ffe50a645
Marc Hoersken <info@marc-hoersken.de>
2014-05-01 21:30:26 +0200

    Replaced XML-based import of Disqus comment with Disqus API

--- management/commands/import_disqus.py
+++ management/commands/import_disqus.py
@@ -1,128 +1,119 @@
 # -*- coding: utf-8 -*-
-from django.utils import text
 from django.db import transaction
+from django.utils import timezone, text
 from django.core.management.base import BaseCommand, CommandError
-from ...models import Author, Thread, Post, Vote
-import xml.etree.ElementTree as ET
+from social.apps.django_app.default.models import UserSocialAuth
+from ...models import User, Author, Thread, Post, Vote
+from disqusapi import DisqusAPI
 import isodate
 
 class Command(BaseCommand):
-    args = '<disqus-export-file.xml>'
-    help = 'Import Disqus comments from XML export file'
+    args = '<disqus-forum> <secret-key> <public-key>'
+    help = 'Import Disqus comments and authors from JSON API'
 
     def handle(self, *args, **options):
-        if len(args) < 1:
-            raise CommandError('Missing argument: <disqus-export-file.xml>')
+        if len(args) < 3:
+            raise CommandError('Missing arguments: <disqus-forum> <secret-key> <public-key>')
 
-        with open(args[0], 'r') as disqus_file:
-            disqus_tree = ET.parse(disqus_file)
-            disqus_root = disqus_tree.getroot()
+        disqus_api = DisqusAPI(args[1], args[2])
+        disqus_forum = args[0]
+        disqus_posts_include = ('unapproved', 'approved', 'spam', 'deleted', 'flagged', 'highlighted')
+        disqus_posts = disqus_api.posts.list(forum=disqus_forum,
+                                             include=disqus_posts_include,
+                                             order='asc')
 
-            with transaction.atomic():
-                self.handle_post(disqus_root)
-
-        self.stdout.write('Successfully imported Disqus comments from XML export file')
-
-    def handle_post(self, disqus_root, parent_id=None, parent=None, depth=0):
-        for post_item in disqus_root.findall('{http://disqus.com}post'):
-            post_parent_item = post_item.find('{http://disqus.com}parent')
-            if not post_parent_item is None:
-                post_parent_id = post_parent_item.attrib['{http://disqus.com/disqus-internals}id']
+        disqus_posts_list = []
+        while len(disqus_posts):
+            disqus_posts_list += disqus_posts.response
+            print 'Fetched', len(disqus_posts_list), 'posts'
+            if disqus_posts.cursor.get('hasNext', False):
+                print 'Fetching more posts'
+                disqus_posts = disqus_api.posts.list(forum=disqus_forum,
+                                                     include=disqus_posts_include,
+                                                     order='asc',
+                                                     cursor=disqus_posts.cursor.get('next'))
             else:
-                post_parent_id = None
+                break
 
+        with transaction.atomic():
+            self.handle_post(disqus_posts_list)
+
+        self.stdout.write('Successfully imported Disqus comments and authors from JSON API')
+
+    def handle_post(self, disqus_posts_list, parent_id=None, parent=None, depth=0):
+        for disqus_post in disqus_posts_list:
+            post_parent_id = disqus_post.get('parent', None)
+            if post_parent_id:
+                post_parent_id = int(post_parent_id)
             if post_parent_id == parent_id:
-                post_id = post_item.attrib['{http://disqus.com/disqus-internals}id']
+                post_id = int(disqus_post.get('id'))
 
                 print '\t'*depth, post_id
 
-                post_message_text = self.get_value_text(post_item, '{http://disqus.com}message')
-                post_created_datetime = self.get_value_datetime(post_item, '{http://disqus.com}createdAt')
+                post_message = disqus_post.get('message')
+                post_created = disqus_post.get('createdAt')
 
-                post_is_deleted_bool     = self.get_value_bool(post_item, '{http://disqus.com}isDeleted',     False)
-                post_is_approved_bool    = self.get_value_bool(post_item, '{http://disqus.com}isApproved',    True )
-                post_is_flagged_bool     = self.get_value_bool(post_item, '{http://disqus.com}isFlagged',     False)
-                post_is_spam_bool        = self.get_value_bool(post_item, '{http://disqus.com}isSpam',        False)
-                post_is_highlighted_bool = self.get_value_bool(post_item, '{http://disqus.com}isHighlighted', False)
+                post_is_deleted     = disqus_post.get('isDeleted',     False)
+                post_is_approved    = disqus_post.get('isApproved',    True )
+                post_is_flagged     = disqus_post.get('isFlagged',     False)
+                post_is_spam        = disqus_post.get('isSpam',        False)
+                post_is_highlighted = disqus_post.get('isHighlighted', False)
 
-                post_author_item = post_item.find('{http://disqus.com}author')
+                disqus_author = disqus_post.get('author')
 
-                author = self.handle_author(post_id, post_author_item)
+                author = self.handle_author(post_id, disqus_author)
 
                 if not parent is None:
                     thread = parent.thread
                 else:
                     thread = Thread.objects.create(category='discussion')
-                    thread.crdate = post_created_datetime
+                    thread.crdate = self.parse_datetime(post_created)
                     thread.save(update_fields=('crdate',))
 
                 post = Post.objects.create(id=post_id, parent=parent, thread=thread, author=author,
-                                           content=post_message_text, is_deleted=post_is_deleted_bool,
-                                           is_approved=post_is_approved_bool, is_flagged=post_is_flagged_bool,
-                                           is_spam=post_is_spam_bool, is_highlighted=post_is_highlighted_bool)
-                post.crdate=post_created_datetime
+                                           content=post_message, is_deleted=post_is_deleted,
+                                           is_approved=post_is_approved, is_flagged=post_is_flagged,
+                                           is_spam=post_is_spam, is_highlighted=post_is_highlighted)
+                post.crdate = self.parse_datetime(post_created)
                 post.save(update_fields=('crdate',))
 
-                self.handle_post(disqus_root, post_id, post, depth+1)
+                self.handle_post(disqus_posts_list, post_id, post, depth+1)
 
-    def handle_author(self, post_id, author_item):
-        author_name_text     = self.get_value_text(author_item, '{http://disqus.com}name'    )
-        author_email_text    = self.get_value_text(author_item, '{http://disqus.com}email'   )
-        author_username_text = self.get_value_text(author_item, '{http://disqus.com}username')
+    def handle_author(self, post_id, disqus_author):
+        author_name         = disqus_author.get('name'       )
+        author_id           = disqus_author.get('id'         , None)
+        author_username     = disqus_author.get('username'   , None)
+        author_is_anonymous = disqus_author.get('isAnonymous', True)
+        author_joined       = disqus_author.get('joinedAt'   , None)
+
+        if author_is_anonymous:
+            author_username = u'anonymous-%d' % post_id
+            author = Author.objects.create_user(username=author_username,
+                                                first_name=author_name)
+            author.is_active = False
+            author.save(update_fields=('is_active',))
+
+            return author
 
         try:
-            if author_username_text:
-                try:
-                    author = Author.objects.get(username=author_username_text, is_active=True)
-                except Author.DoesNotExist:
-                    author = Author.objects.get(email=author_email_text, is_active=True)
-                    author.username = author_username_text
-
-            else:
-                author = Author.objects.get(email=author_email_text, is_active=False)
-
-            if author_email_text:
-                author.email = author_email_text
-            if author_name_text:
-                author.first_name = author_name_text
-
-            author.save()
+            author = Author.objects.get(id=int(author_id),
+                                        username=author_username,
+                                        is_active=True)
 
         except Author.DoesNotExist:
-            if author_username_text:
-                is_active = True
-            else:
-                is_active = False
-                author_username_text = u'disqus-%s-%s' % (post_id, text.slugify(author_name_text))
+            author = Author.objects.create_user(id=int(author_id),
+                                                username=author_username,
+                                                first_name=author_name)
+            author.date_joined = self.parse_datetime(author_joined)
+            author.save(update_fields=('date_joined',))
 
-            author = Author.objects.create_user(username=author_username_text,
-                                                email=author_email_text,
-                                                first_name=author_name_text)
-
-            if not is_active:
-                author.is_active = is_active
-                author.save(update_fields=('is_active',))
+            user = User.objects.get(id=author.id)
+            UserSocialAuth.objects.create(user=user,
+                                          provider='disqus',
+                                          uid=author_id)
 
         print author.username
         return author
 
-    def get_value_text(self, item, name, encoding='utf-8'):
-        if not item is None and not name is None:
-            item = item.find(name)
-            if not item is None and not item.text is None:
-                return unicode(item.text.encode(encoding), encoding=encoding)
-        return None
-
-    def get_value_bool(self, item, name, default):
-        if not item is None and not name is None:
-            item = item.find(name)
-            if not item is None and not item.text is None:
-                return item.text in ('true', '1', True, 1)
-        return default
-
-    def get_value_datetime(self, item, name):
-        if not item is None and not name is None:
-            item = item.find(name)
-            if not item is None and not item.text is None:
-                return isodate.parse_datetime(item.text)
-        return None
+    def parse_datetime(self, datetime):
+        return timezone.make_aware(isodate.parse_datetime(datetime), timezone.utc)
